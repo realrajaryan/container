8f6f39e89b5ff834dd02c4bd05ceaea9ecd4fb89
Danny Canter <danny_canter@apple.com>
2025-09-08 11:35:30 -0700

    Package.swift: Bump CZ to 0.7.0 (#584)
    Closes #585
    
    Has a fix for if a uid doesn't exist in /etc/passwd.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ import PackageDescription
 let releaseVersion = ProcessInfo.processInfo.environment["RELEASE_VERSION"] ?? "0.0.0"
 let gitCommit = ProcessInfo.processInfo.environment["GIT_COMMIT"] ?? "unspecified"
 let builderShimVersion = "0.6.0"
-let scVersion = "0.6.2"
+let scVersion = "0.7.0"
 
 let package = Package(
     name: "container",
