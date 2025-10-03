bfc5ca922286898c0c95334602dc620bacdb785f
J Logan <john_logan@apple.com>
2025-10-03 13:28:16 -0700

    Removes "all rights reserved" from license header. (#711)
    Closes #63.

--- scripts/uninstall-container.sh
+++ scripts/uninstall-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-# Copyright © 2025 Apple Inc. and the container project authors. All rights reserved.
+# Copyright © 2025 Apple Inc. and the container project authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
