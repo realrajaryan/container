a627c82a038d76cc66f12bd064f6724de6b1f990
Danny Canter <danny_canter@apple.com>
2025-11-04 12:33:08 -0800

    Deps: Bump Containerization to 0.13.0 (#848)
    Speeds up unpacks.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ import PackageDescription
 let releaseVersion = ProcessInfo.processInfo.environment["RELEASE_VERSION"] ?? "0.0.0"
 let gitCommit = ProcessInfo.processInfo.environment["GIT_COMMIT"] ?? "unspecified"
 let builderShimVersion = "0.6.3"
-let scVersion = "0.12.1"
+let scVersion = "0.13.0"
 
 let package = Package(
     name: "container",
