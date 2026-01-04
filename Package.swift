028e7e109fc1a36828b33b1df6f5f799ab90da0d
Danny Canter <danny_canter@apple.com>
2026-01-04 10:52:46 -0800

    Deps: Bump Containerization to 0.19.0 (#1015)
    Has read-only rootfs support.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ import PackageDescription
 let releaseVersion = ProcessInfo.processInfo.environment["RELEASE_VERSION"] ?? "0.0.0"
 let gitCommit = ProcessInfo.processInfo.environment["GIT_COMMIT"] ?? "unspecified"
 let builderShimVersion = "0.7.0"
-let scVersion = "0.18.0"
+let scVersion = "0.19.0"
 
 let package = Package(
     name: "container",
